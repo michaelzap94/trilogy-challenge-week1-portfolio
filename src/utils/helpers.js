317ba8056330a96272f9a5bfd4621394dd0e7d93
Michael Zapata <mikeyfriends@hotmail.com>
2022-06-22 00:11:23 +0100

    Added the helpers and assets for About and Resume

---
+++ src/utils/helpers.js
@@ -0,0 +1,9 @@
+export function capitalizeWord(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+export function checkEmailIsValid(str) {
+  var re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(str).toLowerCase());
+}
