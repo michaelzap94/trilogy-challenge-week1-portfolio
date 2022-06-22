2b43984dd23b15ebd8d81c31698424fdee1cd969
Michael Zapata <mikeyfriends@hotmail.com>
2022-06-22 10:56:07 +0100

    added technical skills

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,10 +1,32 @@
 import React from "react";
 import cv from "../../assets/Resume/Michael Zapata CV.pdf";
 
+const SKILLS = [
+  "Java",
+  "TypeScript",
+  "Python",
+  "JavaScript",
+  "Android",
+  "React",
+  "Node.js",
+  "Docker.js",
+  "Django",
+  "SQL",
+  "MongoDB",
+];
+
 const Resume = () => {
   return (
     <section>
-      <a href={cv} download>Download my Resume</a>
+      <a href={cv} download>
+        Download my Resume
+      </a>
+      <h3>Technical Skills</h3>
+      <ul>
+        {SKILLS.map((skill) => (
+          <li>{skill}</li>
+        ))}
+      </ul>
     </section>
   );
 };
